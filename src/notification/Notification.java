06982d577b71f5962843cde3a1cf3bf5ee11b6a6
sokoeurn88 <92101403+sokoeurn88@users.noreply.github.com>
2022-09-03 18:34:10 -0500

    notification

---
+++ src/notification/Notification.java
@@ -0,0 +1,15 @@
+package notification;
+
+public class Notification {
+	WebDriver driver;
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		ChromeOptions chromeoption = new ChromeOptions();
+		
+
+	}
+
+}
